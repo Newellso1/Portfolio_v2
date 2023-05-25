6229a4d1e8f02f09b9015ec10d5347600e3e24d5
Sam <samnewell91@gmail.com>
2023-05-25 19:10:36 +0100

    Tried to add slide-left to each odd slide
    Didnt work, back to drawing board

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+// Function that rounds the corners of container on page load
+const content = document.querySelector('.content')
 window.onload = function () {
     content.classList.add('roundCorners');
 }
 
-const content = document.querySelector('.content')
+// Intersection Observer that shows and hides content if it's out/in view
+
+
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -15,3 +19,6 @@ const observer = new IntersectionObserver((entries) => {
 
 const hiddenElements = document.querySelectorAll('.hidden');
 hiddenElements.forEach((el) => observer.observe(el));
+
+// Function that adds slide-left animation to the h1 elements within the odd sections
+
